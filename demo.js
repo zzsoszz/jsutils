d8197739ea73da9e3a84fa146e181b3c43ffa104
hyq2015 <18030638805@163.com>
2017-03-07 09:54:20 +0800

    update

--- demo.js
+++ demo.js
@@ -190,3 +190,38 @@ function dump_objUp(myObject) {
 	   dump_objUp(myObject.prototype);
   }
 } */
+
+
+
+Date.prototype.format = function (fmt) {
+        var o = {
+            "M+": this.getMonth() + 1, //月份
+            "d+": this.getDate(), //日
+            "h+": this.getHours(), //小时
+            "m+": this.getMinutes(), //分
+            "s+": this.getSeconds(), //秒
+            "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+            "S": this.getMilliseconds() //毫秒
+        };
+        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o)
+            if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        return fmt;
+}
+var time1 = new Date().format("yyyy-MM-dd");
+var time2 = new Date().format("yyyy-MM-dd hh:mm:ss");
+
+
+
+
+
+
+jQuery.prototype.serializeObject=function(){  
+    var obj=new Object();  
+    $.each(this.serializeArray(),function(index,param){  
+        if(!(param.name in obj)){  
+            obj[param.name]=param.value;  
+        }  
+    });  
+    return obj;  
+};
